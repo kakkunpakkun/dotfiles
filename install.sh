87abcce7eba803ab09e18401c08a68dabb2cc048
Junichi Kaku <11kaku@gmail.com>
2022-12-31 15:08:52 +0900

    Fix path

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 echo "start setup..."
 
 ln -s ${SCRIPT_DIR}/.gitconfig ${HOME}/.gitconfig
-ln -s ${SCRIPT_DIR}/.config/config.fish ${HOME}/.config/config.fish
-ln -s ${SCRIPT_DIR}/.config/fish_plugins ${HOME}/.config/fish_plugins
+ln -s ${SCRIPT_DIR}/.config/fish/config.fish ${HOME}/.config/fish/config.fish
+ln -s ${SCRIPT_DIR}/.config/fish/fish_plugins ${HOME}/.config/fish/fish_plugins
 
 echo "done!"
