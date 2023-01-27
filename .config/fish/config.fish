d6b74dc0bd59a557133a2d4862f4ae6633852c11
kp <kakkunpakkun@users.noreply.github.com>
2023-01-27 16:19:29 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,4 +27,5 @@ function git_current_branch
 end
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+set -gx EDITOR "vi"
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
