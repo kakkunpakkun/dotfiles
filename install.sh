40302d2d1f4666b1f92202962e5c64f029fc35ea
kp <kakkunpakkun@users.noreply.github.com>
2023-01-30 22:32:55 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
-#!/bin/bash -ux
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+#!/bin/fish 
+set SCRIPT_DIR (cd (dirname $0); pwd)
 
 echo "start setup..."
 
 echo "install rust..."
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-source "$HOME/.cargo/env"
+set -U fish_user_paths $fish_user_paths $HOME/.cargo/bin
 cargo install exa
 
 echo "install fisher..."
 fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 
 echo "synbolic links..."
-ln -s ${SCRIPT_DIR}/.gitconfig ${HOME}/.gitconfig
-ln -s ${SCRIPT_DIR}/.config/fish/config.fish ${HOME}/.config/fish/config.fish
-rm ${HOME}/.config/fish/fish_plugins
-ln -s ${SCRIPT_DIR}/.config/fish/fish_plugins ${HOME}/.config/fish/fish_plugins
+ln -s {$SCRIPT_DIR}/.gitconfig {$HOME}/.gitconfig
+ln -s {$SCRIPT_DIR}/.config/fish/config.fish {$HOME}/.config/fish/config.fish
+rm {$HOME}/.config/fish/fish_plugins
+ln -s {$SCRIPT_DIR}/.config/fish/fish_plugins {$HOME}/.config/fish/fish_plugins
 
 echo "done!"
